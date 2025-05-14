cb01b26f54adf0e4baa739560e57b7d3b7af0014
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-05-14 22:44:15 +0000

    Start poking external impls.

--- mlir/lib/Dialect/MemRef/IR/MemRefOps.cpp
+++ mlir/lib/Dialect/MemRef/IR/MemRefOps.cpp
@@ -1326,8 +1326,8 @@ Operation *DmaStartOp::cloneWithReindex(RewriterBase &rewriter, Value newSrc,
                                         ValueRange newSrcIndices, Value newDst,
                                         ValueRange newDstIndices) {
   return rewriter.create<DmaStartOp>(
-      getLoc(), newSrc, newSrcIndices, newDst, newDstIndices, getTagMemRef(),
-      getTagIndices(), getStride(), getNumElementsPerStride());
+      getLoc(), newSrc, newSrcIndices, newDst, newDstIndices, getNumElements(),
+      getTagMemRef(), getTagIndices(), getStride(), getNumElementsPerStride());
 }
 
 // ---------------------------------------------------------------------------
@@ -1865,7 +1865,7 @@ Operation *PrefetchOp::cloneWithReindex(RewriterBase &rewriter, Value newMemref,
                                         ValueRange newIndices) {
   return rewriter.create<PrefetchOp>(getLoc(), newMemref, newIndices,
                                      getIsWriteAttr(), getLocalityHintAttr(),
-                                     getisDataCacheAttr());
+                                     getIsDataCacheAttr());
 }
 
 //===----------------------------------------------------------------------===//
